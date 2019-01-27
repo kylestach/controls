00755a6ec5bdfdb30a80845eaffb173eef7186b8
Kyle Stachowicz <kylestach99@gmail.com>
2019-01-27 00:12:25 -0500

    Add NMPC

--- mpc/mpc.cpp
+++ mpc/mpc.cpp
@@ -4,19 +4,20 @@
 int main() {
     Matrix<2> A;
     A << 1.0, 0.01,
-         -0.01, 1.0;
+         0.00, 0.99;
     Matrix<2, 1> B;
     B << 0.0,
          0.01;
     Matrix<2> Q = Matrix<2>::Identity();
+    Q(1, 1) = 0;
     Matrix<1> R = Matrix<1>::Zero();
 
-    Vector<2> x(2, 2);
+    Vector<2> x(0, 1);
 
     ModelPredictiveController<2, 1> mpc(A, B, Vector<1>(-1), Vector<1>(1), Q, R, 100);
     for (int i = 0; i < 1000; i++) {
-        auto u = mpc.control(x, Vector<2>(1.0, 0.0));
-        std::cout << x[0] << ", " << x[1] << ", " << u << std::endl;
+        auto u = mpc.control(x, Vector<2>(0.0, 0.0));
+        std::cout << x(0) << ", " << x(1) << ", " << u(0) << std::endl;
         x = A * x + B * u;
     }
 }
--- mpc/nmpc.cpp
+++ mpc/nmpc.cpp
@@ -1,12 +1,23 @@
 #include "system.h"
 #include "mpc.h"
 
-template<int N, int M, int C, typename System, typename Cost, typename Constraint>
+#include <iostream>
+
+template<int N, int M, int C, typename System, typename Cost, typename CostFinal, typename Constraint>
 class NonlinearModelPredictiveController {
 public:
     NonlinearModelPredictiveController(
-            System sys, Cost cost, Constraint constraint, Vector<C> lb_constraint, Vector<C> ub_constraint, Vector<M> lb_input, Vector<M> ub_input, int L)
-            : problem(M * (L - 1), C * (L - 1)) {
+            System sys, Cost cost, CostFinal cost_final, Constraint constraint, Vector<C> lb_constraint, Vector<C> ub_constraint, Vector<M> lb_input, Vector<M> ub_input, int L)
+            : problem(M * (L - 1), C * (L - 1)),
+              system(sys),
+              constraint(constraint),
+              cost(cost),
+              cost_final(cost_final),
+              lb_constraint(lb_constraint),
+              ub_constraint(ub_constraint),
+              lb_input(lb_input),
+              ub_input(ub_input),
+              L(L) {
         // Minimize sum(i = 0 to L - 1)(1/2[x^T Q x + u^T R u]) + x^T Qf x
         // Subject to x(i + 1) = Ax(i) + Bu(i)
         //
@@ -66,9 +77,13 @@ public:
             x_aug.template block<N, 1>(N * i, 0) = unforced_x;
 
             auto quadratized = quadratize<N, M, Cost>(cost, x0, u0, i);
-            Q_aug.template block<N, N>(N * i, N * i) = quadratized.Q;
+
+            if (i != L - 2) {
+                Q_aug.template block<N, N>(N * i, N * i) = quadratized.Q;
+                q_aug.template block<1, N>(0, N * i) = quadratized.q;
+            }
+
             R_aug.template block<M, M>(M * i, M * i) = quadratized.R;
-            q_aug.template block<1, N>(0, N * i) = quadratized.q;
             r_aug.template block<1, M>(0, M * i) = quadratized.r;
 
             auto constraint_linear = linearize<N, M, Constraint, C>(constraint, x0, u0, i);
@@ -83,12 +98,16 @@ public:
             ub_input_aug.template block<M, 1>(M * i, 0) = ub_input;
         }
 
-        Matrix<> cost_quadratic = R_aug + B_aug.transpose() * Q_aug * B_aug;
-        Matrix<> cost_linear = r_aug + q_aug * B_aug;
+        auto qf = quadratize_final<N, CostFinal>(cost_final, x0);
+        Q_aug.template block<N, N>(N * (L - 2), N * (L - 2)) = qf.Q;
+        q_aug.template block<1, N>(0, N * (L - 2)) = qf.q;
 
-        Matrix<> H_aug = H_u + H_x * x_aug;
-        lb_constraint_aug -= h_const;
-        ub_constraint_aug -= h_const;
+        Matrix<> cost_quadratic = R_aug + B_aug.transpose() * Q_aug * B_aug;
+        Matrix<> cost_linear = r_aug + q_aug * B_aug + x_aug.transpose() * Q_aug * B_aug;
+
+        Matrix<> H_aug = H_u + H_x * B_aug;
+        lb_constraint_aug -= h_const - H_x * x_aug;
+        ub_constraint_aug -= h_const - H_x * x_aug;
 
         qpOASES::Options options;
         options.printLevel = qpOASES::PL_NONE;
@@ -98,43 +117,157 @@ public:
         problem.init(cost_quadratic.data(), cost_linear.data(), H_aug.data(), lb_input_aug.data(), ub_input_aug.data(), lb_constraint_aug.data(), ub_constraint_aug.data(), nWSR, 0);
     }
 
+    void iterate(Vector<N> x0) {
+        Vector<> u0 = Vector<>::Zero(M * (L - 1));
+        problem.getPrimalSolution(u0.data());
+
+        // Do a forward pass similar to the above
+        Matrix<> B_aug = Matrix<>::Zero(N * (L - 1), M * (L - 1));
+        Matrix<> x_aug = Vector<>::Zero(N * (L - 1));
+
+        Matrix<> Q_aug = Matrix<>::Zero(N * (L - 1), N * (L - 1));
+        Matrix<> R_aug = Matrix<>::Zero(M * (L - 1), M * (L - 1));
+        Matrix<> q_aug = Matrix<>::Zero(1, N * (L - 1));
+        Matrix<> r_aug = Matrix<>::Zero(1, N * (L - 1));
+
+        Matrix<> lb_input_aug = Vector<>::Zero(M * (L - 1));
+        Matrix<> ub_input_aug = Vector<>::Zero(M * (L - 1));
+        Matrix<> lb_constraint_aug = Vector<>::Zero(C * (L - 1));
+        Matrix<> ub_constraint_aug = Vector<>::Zero(C * (L - 1));
+
+        Matrix<N> A_i = Matrix<N>::Identity();
+
+        Matrix<> B_aug_row = Matrix<>::Zero(N, M * (L - 1));
+
+        Matrix<> H_x = Matrix<>::Zero(C * (L - 1), N * (L - 1));
+        Matrix<> H_u = Matrix<>::Zero(C * (L - 1), M * (L - 1));
+        Matrix<> h_const = Matrix<>::Zero(C * (L - 1), 1);
+
+        Vector<N> unforced_x = x0;
+
+        Vector<N> x_hat = x0;
+        for (int i = 0; i < (L - 1); i++) {
+            Vector<M> u_hat = u0.template block<M, 1>(M * i, 0);
+
+            auto linearized = linearize<N, M, System>(system, x_hat, u_hat, i);
+            B_aug_row = linearized.A * B_aug_row;
+            B_aug_row.template block<N, M>(0, M * i) = linearized.B;
+            B_aug.template block(N * i, 0, N, M * (L - 1)) = B_aug_row;
+            unforced_x = linearized.A * unforced_x + linearized.c;
+            x_aug.template block<N, 1>(N * i, 0) = unforced_x;
+
+            x_hat = system(x_hat, u_hat, i);
+
+            auto quadratized = quadratize<N, M, Cost>(cost, x_hat, u_hat, i);
+
+            if (i != L - 2) {
+                Q_aug.template block<N, N>(N * i, N * i) = quadratized.Q;
+                q_aug.template block<1, N>(0, N * i) = quadratized.q;
+            }
+
+            R_aug.template block<M, M>(M * i, M * i) = quadratized.R;
+            r_aug.template block<1, M>(0, M * i) = quadratized.r;
+
+            auto constraint_linear = linearize<N, M, Constraint, C>(constraint, x_hat, u_hat, i);
+            H_x.template block<C, N>(C * i, N * i) = constraint_linear.A;
+            H_u.template block<C, M>(C * i, M * i) = constraint_linear.B;
+            h_const.block<C, 1>(C * i, 0) = constraint_linear.c;
+
+            lb_constraint_aug.template block<C, 1>(C * i, 0) = lb_constraint;
+            ub_constraint_aug.template block<C, 1>(C * i, 0) = ub_constraint;
+
+            lb_input_aug.template block<M, 1>(M * i, 0) = lb_input;
+            ub_input_aug.template block<M, 1>(M * i, 0) = ub_input;
+        }
+
+        auto qf = quadratize_final<N, CostFinal>(cost_final, x0);
+        Q_aug.template block<N, N>(N * (L - 2), N * (L - 2)) = qf.Q;
+        q_aug.template block<1, N>(0, N * (L - 2)) = qf.q;
+
+        Matrix<> cost_quadratic = R_aug + B_aug.transpose() * Q_aug * B_aug;
+        Matrix<> cost_linear = r_aug + q_aug * B_aug + x_aug.transpose() * Q_aug * B_aug;
+
+        Matrix<> H_aug = H_u + H_x * B_aug;
+        lb_constraint_aug -= h_const - H_x * x_aug;
+        ub_constraint_aug -= h_const - H_x * x_aug;
+
+        int nWSR = 1000000;
+        problem.hotstart(cost_quadratic.data(), cost_linear.data(), H_aug.data(), lb_input_aug.data(), ub_input_aug.data(), lb_constraint_aug.data(), ub_constraint_aug.data(), nWSR, 0);
+    }
+
+    Vector<> plan(Vector<N> x0, Vector<N> r) {
+        // for (int i = 0; i < 10; i++) {
+             iterate(x0);
+        // }
+        Vector<> u0 = Vector<>::Zero(M * (L - 1));
+        problem.getPrimalSolution(u0.data());
+        return u0;
+    }
+
+    Vector<M> control(Vector<N> x0, Vector<N> r) {
+        for (int i = 0; i < 10; i++) {
+             iterate(x0);
+        }
+        return plan(x0, r).template block<M, 1>(0, 0);
+    }
+
 protected:
     qpOASES::SQProblem problem;
 
     System system;
     Constraint constraint;
+    Cost cost;
+    CostFinal cost_final;
     Vector<C> lb_constraint, ub_constraint;
     Vector<M> lb_input, ub_input;
+
+    int L;
 };
 
 constexpr double dt = 0.01;
 
 struct System {
     Vector<2> operator()(Vector<2> x, Vector<1> u, int t) {
-        return Vector<2>(x(0) + x(1) * dt, x(1) + dt * (u(0) - x(1)));
+        return Vector<2>(x(1), u(0));
     }
 };
 
 struct Cost {
     double operator()(Vector<2> x, Vector<1> u, int t) {
-        return x(0) * x(0);
+        return 0.0 * u(0) * u(0);
+    }
+};
+
+struct CostFinal {
+    double operator()(Vector<2> x) {
+        return 400 * (x(0) - 3) * (x(0) - 3) + 100 * x(1) * x(1);
     }
 };
 
 struct Constraint {
     Vector<1> operator()(Vector<2> x, Vector<1> u, int t) {
-        return Vector<1>(x(0));
+        return Vector<1>(x(1));
     }
 };
 
 int main() {
     System system;
     Cost cost;
+    CostFinal cost_final;
     Constraint constraint;
-    NonlinearModelPredictiveController<2, 1, 1, System, Cost, Constraint> nmpc(
-            system, cost, constraint,
+    Discretized<2, 1, System> disc{dt, system};
+    int N = 500;
+    NonlinearModelPredictiveController<2, 1, 1, Discretized<2, 1, System>, Cost, CostFinal, Constraint> nmpc(
+            disc, cost, cost_final, constraint,
             Vector<1>(-1), Vector<1>(1),
-            Vector<1>(-1), Vector<1>(1),
-            50
+            Vector<1>(-0.5), Vector<1>(0.5),
+            N + 1
         );
+
+    Vector<2> x(0, 0);
+    Vector<> u = nmpc.plan(x, Vector<2>::Zero());
+    for (int i = 0; i < N; i++) {
+        x = disc(x, Vector<1>(u(i)), i);
+        std::cout << x(0) << ", " << x(1) << ", " << u(i) << std::endl;
+    }
 }
